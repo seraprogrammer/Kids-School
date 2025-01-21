45839868b6358aab22a0134334a5653ea639f61e
seraprogrammer <programmernazmul1@gmail.com>
2025-01-21 21:49:55 +0600

    updtae the card and hover also bg-img

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 document.getElementById(
   "date"
-).innerHTML = `<marquee behavior="" direction="right">${new Date().toLocaleDateString(
+).innerHTML = `<marquee behavior="" direction="left">${new Date().toLocaleDateString(
   "en-US",
   { weekday: "long", year: "numeric", month: "long", day: "numeric" }
 )}</marquee>`;
